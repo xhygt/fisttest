5325b4a767f0b04262a9cfb7c9e5e7754af61ef2
lizh-Cloud-20180707 <891968069@qq.com>
2018-07-16 18:30:04 +0800

    origin-dev

--- util/WaitUtil.py
+++ util/WaitUtil.py
@@ -1,4 +1,5 @@
 #encoding = utf-8
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,7 +8,7 @@ class WaitUtil(object):
     #映射定位方式的字典对象
     def __init__(self, driver):
         self.locationTypeDict = {
-            "xpath" : By.XPATH,
+            "xpath": By.XPATH,
             "id" : By.ID,
             "name" : By.NAME,
             "css_selector" : By.CSS_SELECTOR,
@@ -16,5 +17,60 @@ class WaitUtil(object):
             "link_text" : By.LINK_TEXT,
             "partial_link_text" : By.PARTIAL_LINK_TEXT
         }
-    #初始化driver对象
-    self.driver
+        #初始化driver对象
+        self.driver = object
+        #创建显示等待实例对象
+        self.wait = WebDriverWait(self.driver, 30)
+
+    def presenceOfElementLocated(self, locatorMethod, locatorExpression, *arg):
+        # 显示等待页面元素出现在DOM中，但并不一定可见，存在则返回该页面元素对象
+        try:
+            if self.locationTypeDict.has_key(locatorMethod.lower()):
+                element = self.wait.until(
+                    EC.presence_of_element_located((
+                        self.locationTypeDict[locatorMethod.lower()],
+                        locatorExpression)))
+                return element
+            else:
+                raise TypeError(u"未找到定位方式，请确认定位方法是否正确")
+
+        except Exception as e:
+            raise e
+
+    def frameToBeAvailableAndSwitchToIt(self, locationType, locatorExpression, *args):
+        #检查frame是否存在，存在则切换进frame控件中
+        try:
+            self.wait.until(
+                EC.frame_to_be_available_and_switch_to_it((
+                    self.locationTypeDict[locationType.lower()],
+                    locatorExpression)))
+        except Exception as e:
+            #抛出异常信息给上层调用者
+            raise e
+
+    def visibilityOfElementLocated(self, locationType, locatorExpression, *args):
+        # 显示等待页面元素出现在DOM中，并且可见，存在返回该页面元素对象
+        try:
+            self.wait.until(
+                EC.visibility_of_element_located((
+                    self.locationTypeDict[locationType.lower()],
+                    locatorExpression
+                )))
+            return element
+
+        except Exception as e:
+            #抛出异常信息给上层调用者
+            raise e
+if __name__ == '__main__':
+    from selenium import webdriver
+    driver = webdriver.Chrome()
+    driver.get("http://mail.126.com")
+    waitUtil = WaitUtil(driver)
+    waitUtil.frameToBeAvailableAndSwitchToIt("id","x-URS-iframe")
+
+    waitUtil.visibilityOfElementLocated("xpath", "//input[@name='email']")
+
+
+
+
+
