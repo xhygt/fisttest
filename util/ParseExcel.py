9e26924c567cb4602f670d4f22c151dbea44d95e
asd45183 <451878105@qq.com>
2018-07-17 18:14:42 +0800

    File "/Users/dingyq/pyauto/util/ParseExcel.py", line 61, in getRow     print(sheet.rows[rowNo -1]) TypeError: 'generator' object is not subscriptable

--- util/ParseExcel.py
+++ util/ParseExcel.py
@@ -1,4 +1,5 @@
 # encoding =utf-8
+# openpyxl 只支持 .xlsx 格式
 import openpyxl
 from openpyxl.styles import  Border,Side,Font
 import time
@@ -32,10 +33,10 @@ class ParseExcel(object):
     def getSheetByIndex(self,sheetIndex):
         #根据 sheet 的索引号获取该 sheet 对象
         try:
-            sheetname = self.workbook.get_sheet_names()[sheetIndex]
+            sheetname = self.workbook.get_sheet_names()
         except Exception as e:
             raise e
-        sheet = self.workbook.get_sheet_by_name(sheetname)
+        sheet = self.workbook.get_sheet_by_name(sheetname[sheetIndex])
         return sheet
 
     def getRowsNumber(self,sheet):
@@ -54,34 +55,70 @@ class ParseExcel(object):
         # 获取 sheet 中有数据区域的开始列好：
         return  sheet.min_column
 
+    #方法问题，封存。
     def getRow(self,sheet,rowNo):
         # 获取 sheet 中某一行，返回的是这一行所有的数据内容组成的 tuple,
         # 下表从 1 开始，sheet.rows[1]表示第一行
         try:
-            return  sheet.rows[rowNo -1]
+            #print(sheet.rows[rowNo])
+             for cell in list(sheet.rows)[rowNo-1]:
+                print(cell.value)
         except Exception as e:
-            raise  e
+            raise e
+
+    def getRows(self,sheet):
+        #遍历表格中的数据
+        try:
+            for row in sheet.iter_rows():
+                for cell in row:
+                    print(cell.value)
+        except Exception as e:
+            raise e
+
+    def getCellOfValue(self,sheet,coordinate = None,
+                       rowNo = None,colsNo = None):
+        # 根据单元格所在的位置索引获取该单元格中的值，下标从1开始
+        # sheet.cell(row =1 ,column =1 ).value,
+        # 表示  excel 中第一行第一列的值
+        if coordinate !=None:
+            try:
+                return  sheet.cell(coordinate = coordinate).value
+            except Exception as e:
+                raise e
+        elif coordinate is None and rowNo is not None and \
+            colsNo is not None:
+            try:
+                return sheet.cell(row = rowNo,cols = colsNo).value
+            except Exception as e:
+                raise  e
+        else:
+            raise Exception("Insufficient Coordinates of cell !")
+
+
 
 if __name__ == '__main__':
     # 测试代码
+
     pe = ParseExcel()
+    """
     # 测试所需 Excel 文件"XXX.xlsx"请自行创建
-    pe.loadWorkBook(u'/Users/dingyq/Desktop/test.xlsx')
+    pe.loadWorkBook(u'/Users/dingyq/pyauto/datas/test.xlsx')
     print("通过名称获取 sheet 对象的名字:", \
           pe.getSheetByname(u'联系人'))
 
     print("通过 index 序号获取 sheet 对象的名字：", \
           pe.getSheetByIndex(0).title)
-    sheet = pe.getSheetByIndex(0)
+    #sheet = pe.getSheetByIndex(0)
+    sheet = pe.getSheetByname(u'联系人')
     print (type(sheet))
     print(pe.getRowsNumber(sheet)) # 获取最大行
     print(pe.getColsNumber(sheet)) # 获取最大列
-    rows = pe.getRow(sheet,1) # 获取第一行
-    for i in rows:
-        print(i.value)
+
     #print(pe.getCellofValue(sheet,rowNo = 1, colsNo = 1))
     #pe.writeCell(sheet,u'我爱祖国', rowNo = 10, colsNo = 10)
     #pe.writeCellCurrentTime(sheet,rowNo = 10 ,colsNo =10)
-
-
+"""
+    pe.loadWorkBook(u'/Users/dingyq/pyauto/datas/test.xlsx')
+    sheet = pe.getSheetByIndex(0)
+    pe.getRow(sheet,1)
 
---
+++ util/xlrd1.py
@@ -0,0 +1,70 @@
+"""
+# 需要修改
+import xlrd
+import xlwt
+class ParseExcel(object):
+
+
+    def __init__(self):
+        self.workbook = None
+        self.excelFile = None
+        self.font = Font(color = None) # 设置字体的颜色
+        # 颜色对应的 RGB 值
+            self.RGBDict = {'red': 'FFFF3030', 'green':'FF008B00'}
+
+    def loadWorkBook(self,excelPathAndName):
+        # 将 Excel 文件加载到内存，并获取其 workbook 对象
+        try:
+            self.workbook = xlrd.open_workbook(excelPathAndName)
+        except Exception as e:
+            raise e
+        self.excelfile = excelPathAndName
+        return  self.workbook
+
+    def getSheetByname(self,sheetName):
+        # 根据 sheet 名获取该 sheet 对象
+        try:
+            sheet = self.workbook.sheet_by_name(sheetName)
+            #sheet = self.workbook.sheet_names()
+            return  sheet
+        except Exception as e:
+            raise e
+    def getSheetByIndex(self,sheetIndex):
+        #根据 sheet 的索引号获取该 sheet 对象
+        try:
+            sheetname = self.workbook.sheet_loaded(sheetIndex)
+        except Exception as e:
+            raise e
+        sheet = self.workbook.sheet_by_name(sheetname)
+        return sheet
+    def getRowsNumber(self,sheet):
+        # 获取 sheet 中有数据区域的结束行号
+        return  sheet.max_row
+
+    def getColsNumber(self,sheet):
+        # 获取 sheet 中有数据区域的结束列号
+        return  sheet.max_column
+if __name__ == '__main__':
+    # 测试代码
+    pe = ParseExcel()
+    # 测试所需 Excel 文件"XXX.xlsx"请自行创建
+    pe.loadWorkBook(u'/Users/dingyq/pyauto/datas/test.xlsx')
+    print("通过名称获取 sheet 对象的名字:", \
+          pe.getSheetByname(u'联系人'))
+    sheet = pe.getSheetByIndex(0)
+    print(pe.getColsNumber(sheet))
+    print(pe.getRowsNumber(sheet))
+
+    #print("通过 index 序号获取 sheet 对象的名字：", \
+         # pe.getSheetByIndex(0).title)
+   # sheet = pe.getSheetByIndex(0)
+    #print (type(sheet))
+    #print(pe.getRowsNumber(sheet)) # 获取最大行
+    #print(pe.getColsNumber(sheet)) # 获取最大列
+    #错误开始
+    #rows = pe.getRow(sheet,1) # 获取第一行
+    ##   print(i.value)
+    #print(pe.getCellofValue(sheet,rowNo = 1, colsNo = 1))
+    #pe.writeCell(sheet,u'我爱祖国', rowNo = 10, colsNo = 10)
+    #pe.writeCellCurrentTime(sheet,rowNo = 10 ,colsNo =10)
+"""
